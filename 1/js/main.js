f700d6931029fcee41d7c83b4034124fde1ff0c9
abmann18 <ashley.b.mann@gmail.com>
2015-04-13 11:56:48 -0400

    Test commit

--- 1/js/main.js
+++ 1/js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
 	$(window).load(function(){
 
+		$("input").keyup(multiplyInputs);
+  			function multiplyInputs() {
+       			var multiply = 0;
+
+       	$("input").each(function () {           
+        	var $textfield1 = $('.value1', this).val();
+         	var $textfield2 = $('.value2', this).val();
+        	var $textfield3 = $('.value3', $(this).next()).val();
+
+        	var $total = $value1 * $value2 * $value3;
+
+	     	});
+
+       $("#grandTotal").text(multiplyInputs);
+   }
 	});
 });
